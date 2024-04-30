1e014f4dec5ee471c6296aadb010c8a58b68adf0
SheaOdoherty <143014763+SheaOdoherty@users.noreply.github.com>
2024-04-29 23:19:21 -0700

    Update CheckDigit.java

--- src/main/java/CheckDigit.java
+++ src/main/java/CheckDigit.java
@@ -8,7 +8,7 @@ public class CheckDigit
    public static int getCheck(int num) {
     int sum = 0;
     for (int i = 1; i <= getNumberOfDigits(num); i++) {
-        sum+= (8-i) * getDigit(num,i);
+        sum += (8-i) * getDigit(num, i);
     }
         return sum % 10;
 }
@@ -31,7 +31,7 @@ public class CheckDigit
          return false;
        }
      }
-   }    
+       
    
    /** Returns the number of digits in num. */    
    public static int getNumberOfDigits(int num)    
